9dfaf1a86078ea524cb765116176fca5dafa88ae
jnpinzonv <jnpinzonv@bc06dba5-b48c-9925-9adc-79fba4acdb5a>
2014-10-08 11:37:01 +0000

    commit inicial

--- trunk/demos/hammerlab/web/src/main/java/co/com/hammerlab/controller/EmpresaController.java
+++ trunk/demos/hammerlab/web/src/main/java/co/com/hammerlab/controller/EmpresaController.java
@@ -27,306 +27,345 @@ import co.com.hammerlab.util.ConstantesUtil;
 @ConversationScoped
 public class EmpresaController implements Serializable {
 
-    /**
-     * Serialziacion
-     */
-    private static final long serialVersionUID = -8515841786261886008L;
+	/**
+	 * Serialziacion
+	 */
+	private static final long serialVersionUID = -8515841786261886008L;
 
-    @Inject
-    private Conversation conversation;
-    /**
-     * Inyeccion de contexto de faces
-     */
-    @Inject
-    private FacesContext facesContext;
-    /**
-     * Realcion con el EJB que controla las transacciones del objeto
-     */
-    @Inject
-    private EmpresaBean empresaBean;
-    /**
-     * Entidad sobre la que se gestiona la transaccion
-     */
-    private Empresa newObject;
-    /**
-     * Variable de control
-     */
-    private boolean editMode;
+	@Inject
+	private Conversation conversation;
+	/**
+	 * Inyeccion de contexto de faces
+	 */
+	@Inject
+	private FacesContext facesContext;
+	/**
+	 * Realcion con el EJB que controla las transacciones del objeto
+	 */
+	@Inject
+	private EmpresaBean empresaBean;
+	/**
+	 * Entidad sobre la que se gestiona la transaccion
+	 */
+	private Empresa newObject;
+	/**
+	 * Variable de control
+	 */
+	private boolean editMode;
 
-    /**
-     * Variable de control de conversacion
-     */
-    private boolean bandera = Boolean.FALSE;
+	/**
+	 * Variable de control de conversacion
+	 */
+	private boolean bandera = Boolean.FALSE;
 
-    /**
-     * Devuelve el valor de newUsuario
-     * 
-     * @return El valor de newUsuario
-     */
-    public Empresa getNewObject() {
-        return newObject;
-    }
+	/**
+	 * Devuelve el valor de newUsuario
+	 * 
+	 * @return El valor de newUsuario
+	 */
+	public Empresa getNewObject() {
+		return newObject;
+	}
 
-    /**
-     * 
-     */
-    private List<Empresa> listaEmpresa;
-
-    private String refer;
-
-    private Empresa empresaSelect;
-
-    /**
+	/**
      * 
      */
-    private List<Empresa> selectEmpresas;
+	private List<Empresa> listaEmpresa;
 
-    public void busqueda() {
-        listaEmpresa = empresaBean.getAll();
-    }
-
-    public String empresaSeleccionada() {
-        empresaSelect = selectEmpresas.get(0);
-        return ConstantesUtil.REFER;
-    }
-
-    /**
-     * Asigna el valor del objeto seleccionado pra su edicion
+	/**
      * 
-     * @param userId
-     *            Identificador del objeto Seleccionado
-     * @return Retorna regla de nevagacion
      */
-    public String initEditarModo() {
-        editMode = Boolean.TRUE;
-        newObject = empresaBean.getByID(selectEmpresas.get(0).getId());
-        return ConstantesUtil.CREAR_ACTU;
-    }
+	private String refer;
 
-    public String initVistaModo() {
-        newObject = empresaBean.getByID(selectEmpresas.get(0).getId());
-        return ConstantesUtil.VER;
-    }
-
-    public String initCrearModo() {
-        return ConstantesUtil.CREAR_ACTU;
-    }
-
-    public String cancelar() {
-        return ConstantesUtil.ATRAS;
-    }
-
-    public String reiniciar() {
-        return ConstantesUtil.ATRAS;
-    }
-
-    /**
-     * Actualiza un objeto en la base de datos
+	/**
      * 
-     * @return Retorna regla de nevagacion
      */
-    public String actualizar() {
-        try {
-            empresaBean.update(newObject);
-            editMode = Boolean.FALSE;
-            initNewObject();
-        } catch (Exception e) {
-            String errorMessage = getRootErrorMessage(e);
-            addMessage(FacesMessage.SEVERITY_ERROR, errorMessage);
-        }
+	private Empresa empresaSelect;
 
-        return ConstantesUtil.ATRAS;
-    }
-
-    /**
-     * Elimina un objeto en base de datos
+	/**
      * 
-     * @param id
-     *            Identificador del objeto a eliminar
-     * @return Retorna regla de nevagacion
      */
-    public void eliminar(Long idObject) {
-        try {
-            empresaBean.delete(idObject);
-            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Empresa Eliminada!", "Exito!!"));
-        } catch (Exception e) {
-            String errorMessage = getRootErrorMessage(e);
-            addMessage(FacesMessage.SEVERITY_ERROR, errorMessage);
+	private List<Empresa> selectEmpresas;
 
-        }
-    }
-
-    /**
-     * Registra un nuevo objeto en Base de datos
+	/**
      * 
-     * @return Retorna regla de nevagacion
-     * @throws Exception
-     *             Lanza una excepcion si hay un error en la transacciòn
      */
-    public String crear() {
-        try {
-            empresaBean.save(newObject);
-            facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito!", "Se guardo un registro de una Empresa"));
-            initNewObject();
-            busqueda();
-            return ConstantesUtil.ATRAS;
-        } catch (Exception e) {
-            String errorMessage = getRootErrorMessage(e);
-            addMessage(FacesMessage.SEVERITY_ERROR, errorMessage);
+	public void busqueda() {
+		listaEmpresa = empresaBean.getAll();
+	}
 
-        }
-        return "";
-    }
+	/**
+	 * @return
+	 */
+	public String empresaSeleccionada() {
+		empresaSelect = selectEmpresas.get(0);
+		return ConstantesUtil.REFER;
+	}
 
-    public String visualizar() {
-        return ConstantesUtil.VER;
-    }
+	/**
+	 * Asigna el valor del objeto seleccionado pra su edicion
+	 * 
+	 * @param userId
+	 *            Identificador del objeto Seleccionado
+	 * @return Retorna regla de nevagacion
+	 */
+	public String initEditarModo() {
+		editMode = Boolean.TRUE;
+		newObject = empresaBean.getByID(selectEmpresas.get(0).getId());
+		return ConstantesUtil.CREAR_ACTU;
+	}
 
-    /**
-     * Inicializa el bakinbean de control
-     */
-    public void initNewObject() {
+	/**
+	 * @return
+	 */
+	public String initVistaModo() {
+		newObject = empresaBean.getByID(selectEmpresas.get(0).getId());
+		return ConstantesUtil.VER;
+	}
 
-        if (bandera == Boolean.FALSE) {
-            if (conversation.isTransient()) {
-                conversation.begin();
-            }
-            bandera = Boolean.TRUE;
-            busqueda();
-            selectEmpresas=null;
-            // TODO pendienta para inicializar el estado de editaod se deve mover
-            if (editMode == Boolean.FALSE) {
-                newObject = new Empresa();
-            }
+	/**
+	 * @return
+	 */
+	public String initCrearModo() {
+		editMode = Boolean.FALSE;
+		newObject = new Empresa();
+		return ConstantesUtil.CREAR_ACTU;
+	}
 
-        }
+	/**
+	 * @return
+	 */
+	public String cancelar() {
+		return ConstantesUtil.ATRAS;
+	}
 
-    }
+	/**
+	 * @return
+	 */
+	public String reiniciar() {
+		busqueda();
+		return ConstantesUtil.ATRAS;
+	}
 
-    /**
-     * Realiza transformacion de mensaje para el control del log
-     * 
-     * @param e
-     *            Excepcion generada
-     * @return Retorna el mensaje de error
-     */
-    private String getRootErrorMessage(Exception e) {
-        // Default to general error message that registration failed.
-        String errorMessage = "Registration failed. See server log for more information";
-        if (e == null) {
-            // This shouldn't happen, but return the default messages
-            return errorMessage;
-        }
+	/**
+	 * Actualiza un objeto en la base de datos
+	 * 
+	 * @return Retorna regla de nevagacion
+	 */
+	public String actualizar() {
+		try {
+			empresaBean.update(newObject);
+			editMode = Boolean.FALSE;
+			addMessage(FacesMessage.SEVERITY_INFO, "El registro del cliente fue actualizado");
+			newObject = new Empresa();
+			busqueda();
+		} catch (Exception e) {
+			String errorMessage = getRootErrorMessage(e);
+			addMessage(FacesMessage.SEVERITY_ERROR, errorMessage);
+		}
 
-        // Start with the exception and recurse to find the root cause
-        Throwable t = e;
-        while (t != null) {
-            // Get the message from the Throwable class instance
-            errorMessage = t.getLocalizedMessage();
-            t = t.getCause();
-        }
-        // This is the root cause message
-        return errorMessage;
-    }
+		return ConstantesUtil.ATRAS;
+	}
 
-    /**
-     * Genera un mensaje al contexto
-     * 
-     * @param severidad
-     *            , Severidad del mensaje
-     * @param mensaje
-     *            Clave del mensaje
-     */
-    public void addMessage(Severity severidad, String mensaje) {
-        facesContext.addMessage(null, new FacesMessage(severidad, "", mensaje));
+	/**
+	 * Elimina un objeto en base de datos
+	 * 
+	 * @param id
+	 *            Identificador del objeto a eliminar
+	 * @return Retorna regla de nevagacion
+	 */
+	public String eliminar() {
+		try {
+			for (Empresa element : selectEmpresas) {
+				empresaBean.delete(element.getId());
+			}
+			
+			if(selectEmpresas.size()>0){
+				addMessage(FacesMessage.SEVERITY_INFO, "Los clientes han sido eliminados");
+			}else{
+				addMessage(FacesMessage.SEVERITY_INFO, "El cliente a sido eliminado");
+			}
+			busqueda();
+			selectEmpresas=null;
+			
+		} catch (Exception e) {
+			String errorMessage = getRootErrorMessage(e);
+			addMessage(FacesMessage.SEVERITY_ERROR, errorMessage);
 
-    }
+		}
+		
+		return "";
+	}
 
-    /**
-     * Lista de objetos a ser consultados y visualizados en pantalla
-     * 
-     * @return Retorna una lista de obejtos
-     */
-    public List<Empresa> getListaEmpresa() {
-        return listaEmpresa;
-    }
+	/**
+	 * Registra un nuevo objeto en Base de datos
+	 * 
+	 * @return Retorna regla de nevagacion
+	 * @throws Exception
+	 *             Lanza una excepcion si hay un error en la transacciòn
+	 */
+	public String crear() {
+		try {
+			empresaBean.save(newObject);
+			addMessage(FacesMessage.SEVERITY_INFO,"Se guardo un registro de un Cliente");
+			newObject = new Empresa();
+			busqueda();
+			return ConstantesUtil.ATRAS;
+		} catch (Exception e) {
+			String errorMessage = getRootErrorMessage(e);
+			addMessage(FacesMessage.SEVERITY_ERROR, errorMessage);
 
-    /**
-     * @return the selectEmpresas
-     */
-    public List<Empresa> getSelectEmpresas() {
-        if (selectEmpresas == null) {
-            selectEmpresas = new ArrayList<Empresa>();
-        }
-        return selectEmpresas;
-    }
+		}
+		return "";
+	}
 
-    /**
-     * @param selectEmpresas
-     *            the selectEmpresas to set
-     */
-    public void setSelectEmpresas(List<Empresa> selectEmpresas) {
-        this.selectEmpresas = selectEmpresas;
-    }
+	public String visualizar() {
+		return ConstantesUtil.VER;
+	}
 
-    /**
-     * @param listaEmpresa
-     *            the listaEmpresa to set
-     */
-    public void setListaEmpresa(List<Empresa> listaEmpresa) {
-        this.listaEmpresa = listaEmpresa;
-    }
+	/**
+	 * Inicializa el bakinbean de control
+	 */
+	public void initNewObject() {
 
-    /**
-     * @return the editMode
-     */
-    public boolean isEditMode() {
-        return editMode;
-    }
+		if (bandera == Boolean.FALSE) {
+			if (conversation.isTransient()) {
+				conversation.begin();
+			}
+			bandera = Boolean.TRUE;
+			busqueda();
+			selectEmpresas = null;
+			newObject = new Empresa();
 
-    /**
-     * @param editMode
-     *            the editMode to set
-     */
-    public void setEditMode(boolean editMode) {
-        this.editMode = editMode;
-    }
+		}
 
-    /**
-     * Devuelve el valor de refer
-     * 
-     * @return El valor de refer
-     */
-    public String getRefer() {
-        return refer;
-    }
+	}
 
-    /**
-     * Establece el valor de refer
-     * 
-     * @param refer
-     *            El valor por establecer para refer
-     */
-    public void setRefer(String refer) {
-        this.refer = refer;
-    }
+	/**
+	 * Realiza transformacion de mensaje para el control del log
+	 * 
+	 * @param e
+	 *            Excepcion generada
+	 * @return Retorna el mensaje de error
+	 */
+	private String getRootErrorMessage(Exception e) {
+		// Default to general error message that registration failed.
+		String errorMessage = "Registration failed. See server log for more information";
+		if (e == null) {
+			// This shouldn't happen, but return the default messages
+			return errorMessage;
+		}
 
-    /**
-     * Devuelve el valor de empresaSelect
-     * @return El valor de empresaSelect
-     */
-    public Empresa getEmpresaSelect() {
-        return empresaSelect;
-    }
+		// Start with the exception and recurse to find the root cause
+		Throwable t = e;
+		while (t != null) {
+			// Get the message from the Throwable class instance
+			errorMessage = t.getLocalizedMessage();
+			t = t.getCause();
+		}
+		// This is the root cause message
+		return errorMessage;
+	}
 
-    /**
-     * Establece el valor de empresaSelect
-     * @param empresaSelect El valor por establecer para empresaSelect
-     */
-    public void setEmpresaSelect(Empresa empresaSelect) {
-        this.empresaSelect = empresaSelect;
-    }
-    
-    
+	/**
+	 * Genera un mensaje al contexto
+	 * 
+	 * @param severidad
+	 *            , Severidad del mensaje
+	 * @param mensaje
+	 *            Clave del mensaje
+	 */
+	public void addMessage(Severity severidad, String mensaje) {
+		facesContext.addMessage(null, new FacesMessage(severidad, "", mensaje));
+
+	}
+
+	/**
+	 * Lista de objetos a ser consultados y visualizados en pantalla
+	 * 
+	 * @return Retorna una lista de obejtos
+	 */
+	public List<Empresa> getListaEmpresa() {
+		return listaEmpresa;
+	}
+
+	/**
+	 * @return the selectEmpresas
+	 */
+	public List<Empresa> getSelectEmpresas() {
+		if (selectEmpresas == null) {
+			selectEmpresas = new ArrayList<Empresa>();
+		}
+		return selectEmpresas;
+	}
+
+	/**
+	 * @param selectEmpresas
+	 *            the selectEmpresas to set
+	 */
+	public void setSelectEmpresas(List<Empresa> selectEmpresas) {
+		this.selectEmpresas = selectEmpresas;
+	}
+
+	/**
+	 * @param listaEmpresa
+	 *            the listaEmpresa to set
+	 */
+	public void setListaEmpresa(List<Empresa> listaEmpresa) {
+		this.listaEmpresa = listaEmpresa;
+	}
+
+	/**
+	 * @return the editMode
+	 */
+	public boolean isEditMode() {
+		return editMode;
+	}
+
+	/**
+	 * @param editMode
+	 *            the editMode to set
+	 */
+	public void setEditMode(boolean editMode) {
+		this.editMode = editMode;
+	}
+
+	/**
+	 * Devuelve el valor de refer
+	 * 
+	 * @return El valor de refer
+	 */
+	public String getRefer() {
+		return refer;
+	}
+
+	/**
+	 * Establece el valor de refer
+	 * 
+	 * @param refer
+	 *            El valor por establecer para refer
+	 */
+	public void setRefer(String refer) {
+		this.refer = refer;
+	}
+
+	/**
+	 * Devuelve el valor de empresaSelect
+	 * 
+	 * @return El valor de empresaSelect
+	 */
+	public Empresa getEmpresaSelect() {
+		return empresaSelect;
+	}
+
+	/**
+	 * Establece el valor de empresaSelect
+	 * 
+	 * @param empresaSelect
+	 *            El valor por establecer para empresaSelect
+	 */
+	public void setEmpresaSelect(Empresa empresaSelect) {
+		this.empresaSelect = empresaSelect;
+	}
 
 }
