472eca98db254de72cc6e845aac7ed5ae13f8748
luisarturo1989 <luisarturo1989@gmail.com>
2014-10-11 17:48:53 +0000

    CU: [N/A] IN: [N/A] ES: [Finalizado] OB: [Se crea la clase encargada de albergar las Utilidades almacenadas en cuanto a fechas]

--- demos/hammerlab/web/src/main/java/co/com/hammerlab/util/FechaUtils.java
+++ demos/hammerlab/web/src/main/java/co/com/hammerlab/util/FechaUtils.java
@@ -4,6 +4,7 @@
 package co.com.hammerlab.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -16,17 +17,33 @@ public class FechaUtils {
 	 * Constante encargada de almacena el formato dd/MM/yyyy
 	 */
 	private static final String FORMATO_FECHA = "dd/MM/yyyy";
+	
+	private static final String FORMATO_HORA = "HH:mm:ss";
 
 	/**
 	 * Método encargado de formatear la fecha conforme a la constante FORMATO_FECHA
 	 * @param fecha, Fecha a formatear
 	 * @return fecha formateada
 	 */
-	public static String formatearfecha(Date fecha) {
+	public static String formatearfechaSinHora(Date fecha) {
 		if (fecha != null) {
 			SimpleDateFormat dateFormat = new SimpleDateFormat(FORMATO_FECHA);			
 			return dateFormat.format(fecha);
 		}
 		return null;
 	}
+	
+	/**
+	 * Método encargado de formatear la fecha conforme a la constante FORMATO_FECHA
+	 * @param fecha, Fecha a formatear
+	 * @return fecha formateada
+	 */
+	public static String formatearfechaConHora(Date fecha) {
+		if (fecha != null) {
+			SimpleDateFormat dateFormat = new SimpleDateFormat(FORMATO_FECHA+" "+FORMATO_HORA);			
+			return dateFormat.format(fecha);
+		}
+		return null;
+	}
+	
 }
