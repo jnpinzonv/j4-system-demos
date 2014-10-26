1fefadf8ac68d57c0b348077d4c6d9c6b17b3120
luisarturo1989 <luisarturo1989@gmail.com>
2014-10-26 20:55:01 +0000

    CU: [N/A] IN: [N/A] ES: [Finalizado] OB: [se agregan artefactos del reporte]

---
+++ demos/hammerlab/web/src/main/java/co/com/hammerlab/util/ReporteConstants.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package co.com.hammerlab.util;
+
+/**
+ * @author Lgartz
+ */
+public interface ReporteConstants {
+
+	String DIRECCION = "reporte_direccion";
+	
+	String TELEFONOS = "reporte_telefonos";
+	
+	String EMAIL = "reporte_email";
+	
+	String WEB = "reporte_web";
+	
+	String RUTA = "reporte_ruta";
+}
